7976fd8517fd1af2ae8520598820f6d1528c2168
Kathryn Steib <kate.steib@gmail.com>
2019-11-07 12:24:53 -0600

    Studio 2.5 completed and fixed bonus missions.

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,4 +1,30 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        boolean finished = false;
+
+        Scanner inputRadius = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter a radius: ");
+            if (inputRadius.hasNextDouble()) {
+                radius = inputRadius.nextDouble();
+                if (radius > 0) {
+                    area = Circle.getArea(radius);
+                    System.out.println("The area of a circle of radius " + radius + " is: " + area);
+                    finished = true;
+                }
+            } else {
+                System.out.println("Input is not a number.");
+                finished = true;
+            }
+        }
+        while (!finished);
+
+    }
 }
--- src/org/launchcode/java/studios/areaofacircle/Circle.java
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -1,4 +1,9 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.lang.Math.*; // Imports Math constants
+
 public class Circle {
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius; // Can use Math.PI instead of 3.14 for a more accurate calculation.
+    }
 }
